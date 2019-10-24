51fcb5c61c74f42c4538d8f6edbd0f75fdcc361f
Bruno <bro_coly@hotmail.fr>
2019-10-24 10:04:45 +0100

    added argument generator and new examples

--- script.sh
+++ script.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# for ((s=1000; s<=2000; s+=50))
+ # for s in 25000 50000 75000 100000
+ # do
+ # java -jar def-generator.jar -p $s T > Examples/Chains/Strict/b$s.txt
+ # java -jar def-generator.jar -p $s F > Examples/Chains/Defeasible/b$s.txt
+ # java -jar def-generator.jar -c $s T > Examples/Cycles/Strict/b$s.txt
+ # java -jar def-generator.jar -c $s F > Examples/Cycles/Defeasible/b$s.txt
+ # done
+
+# for s in 8 9 10
 # do
-# java -jar def-generator.jar -p $s T > Examples/Chains/Strict/b$s.txt
-# java -jar def-generator.jar -p $s F > Examples/Chains/Defeasible/b$s.txt
-# java -jar def-generator.jar -c $s T > Examples/Cycles/Strict/b$s.txt
-# java -jar def-generator.jar -c $s F > Examples/Cycles/Defeasible/b$s.txt
-# java -jar def-generator.jar -l $s T > Examples/Levels/Priority/b$s.txt
-# java -jar def-generator.jar -l $s F > Examples/Levels/No_priority/b$s.txt
+# java -jar def-generator.jar -t 3 $s F > Examples/Trees/b$s-3.txt
 # done
 
-for ((s=1; s<=10; s+=1))
+for s in 10 11 12 13 14 15 20 1000 5000 10000 30000
 do
-java -jar def-generator.jar -t 3 $s T > Examples/Trees/Strict/b$s.txt
-java -jar def-generator.jar -t 3 $s F > Examples/Trees/Defeasible/b$s.txt
-java -jar def-generator.jar -m $s > Examples/Teams/b$s.txt
+java -jar def-generator.jar -l $s F > Examples/Levels/Normal/b$s.txt
 done
+
+# for s in 10 1000 5000 10000 30000
+# do
+# java -jar def-generator.jar -l $s T > Examples/Levels/Superiority/b$s.txt
+# done
+#
+# for s in 3 4 5 6 7
+# do
+#   java -jar def-generator.jar -m $s > Examples/Teams/b$s.txt
+# done
